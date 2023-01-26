530560959de251d27e280a7902bd612fe53e36fc
Michael Martinez <Michael.martinez1@calbaptist.edu>
2023-01-25 16:38:41 -0800

    first commit

---
+++ src/HtmlValidator.java
@@ -0,0 +1,24 @@
+/*Michael Martinez
+    Created: 01/25/2023
+    Data Structures and Analysis
+    Homework #1: HTML Validator
+    Due: Monday, 1/26/23, 10 PM
+ */
+public class HtmlValidator {
+    //IMPLEMENT YOUR CODE HERE
+    public void addTag(HtmlTag tag){
+
+    }
+
+    public Queue<HtmlTag> getTags(){
+
+    }
+
+    public void removeAll(String element){
+
+    }
+
+    public void validate(){
+
+    }
+}
