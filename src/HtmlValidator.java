cbc72bbb2bb2f691fc2cb715d5dbc467ba4ba1a5
Michael Martinez <Michael.martinez1@calbaptist.edu>
2023-01-26 19:49:44 -0800

    HtmlValidator and Tests

--- src/HtmlValidator.java
+++ src/HtmlValidator.java
@@ -1,24 +1,112 @@
+
 /*Michael Martinez
     Created: 01/25/2023
     Data Structures and Analysis
     Homework #1: HTML Validator
     Due: Monday, 1/26/23, 10 PM
  */
-public class HtmlValidator {
-    //IMPLEMENT YOUR CODE HERE
-    public void addTag(HtmlTag tag){
 
+
+import java.util.*;
+public class HtmlValidator {
+
+    public static final String INDENTATION_MARKER = "    ";
+    private Queue<HtmlTag> tags;
+    public HtmlValidator(){
+        tags = new LinkedList<>();
+     }
+
+    public HtmlValidator(Queue<HtmlTag> tags){
+        if(tags == null){
+            throw new IllegalArgumentException("YOU CANT DO THAT HERE");
+        }
+        this.tags = new LinkedList<>(tags);
+    }
+    public void addTag(HtmlTag tag){
+        if(tag == null){
+            throw new IllegalArgumentException("Cannot add null tag!");
+        }
+        tags.add(tag);
     }
 
     public Queue<HtmlTag> getTags(){
-
+        return new LinkedList<>(tags);
     }
 
     public void removeAll(String element){
 
+
+        if(element == null){
+            throw new IllegalArgumentException("Cannot add null element");
+        }
+        tags.removeIf(tag ->tag.getElement().equalsIgnoreCase(element));
+
+        /*while(!tags.isEmpty()){
+
+            element.equals(tags.remove());
+        }*/
+
+
     }
 
     public void validate(){
+        Stack<HtmlTag> openTags = new Stack<>();
+        for(int i = 0; i < tags.size(); i++) {
+            // for(HtmlTag tag : tags){ is NOT allowed by the spec
+            // Because we can't use a foreach loop :l
+            HtmlTag tag = tags.remove();
+            tags.add(tag);
 
+            if (tag.isSelfClosing()) {
+                printWithIndentation(tag, openTags.size());
+            } else if (tag.isOpenTag()) {
+                printWithIndentation(tag, openTags.size());
+                openTags.push(tag);
+            } else if (!openTags.isEmpty() && tag.matches(openTags.peek())) { // By exhaustion, the tag must be a closing tag
+                // Closing tag should be at same depth as opening, so we pop before printing
+                openTags.pop();
+                printWithIndentation(tag, openTags.size());
+            } else {
+                System.out.println("ERROR unexpected tag: " + tag.toString());
+            }
+        }
+        // Deal with unclosed tags
+        while (!openTags.isEmpty()) {
+            HtmlTag tag = openTags.pop();
+            System.out.println("ERROR unclosed tag: " + tag.toString());
+        }
+        /*
+        Stack<HtmlTag> openTags = new Stack<>();
+        for(int i = 0; i < tags.size(); i++){
+            HtmlTag tag = tags.remove();
+            tags.add(tag);
+
+            if(tag.isSelfClosing()){
+                printWithIndentation(tag, openTags.size());
+            } else if(tag.isOpenTag()){
+                printWithIndentation(tag, openTags.size());
+            } else if(!openTags.isEmpty() && tag.matches(openTags.peek())){
+                openTags.pop();
+                printWithIndentation(tag, openTags.size());
+            }
+            else{
+                System.out.println("Error! Unexpected tag: " + tag.toString());
+            }
+        }
+
+        while(!openTags.isEmpty()){
+            HtmlTag tag = openTags.pop();
+            System.out.println("Error unclosed tag:" + tag.toString());
+        }*/
+
+    }
+
+    private static void printWithIndentation(HtmlTag tag, int indentationLevel){
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < indentationLevel; i++){
+            sb.append(INDENTATION_MARKER);
+        }
+        sb.append(tag.toString());
+        System.out.println(sb.toString());
     }
 }
--- tst/HtmlValidatorTest.java
+++ tst/HtmlValidatorTest.java
@@ -13,7 +13,9 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintStream;
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
 
 public class HtmlValidatorTest {
     /**
@@ -67,7 +69,7 @@ public class HtmlValidatorTest {
         Queue<HtmlTag> tags = new LinkedList<>();
         tags.add(new HtmlTag("b", true));      // <b>
         tags.add(new HtmlTag("b", false));     // </b>
-        tags.add(new HtmlTag("br"));           // <br/>
+        tags.add(new HtmlTag("br",true));           // <br/>
         HtmlValidator validator = new HtmlValidator(tags);
 
         //Note test0_expected_output.txt is placed under tst. Use relative path!
@@ -82,6 +84,18 @@ public class HtmlValidatorTest {
      */
 	@Test
 	public void test1(){
+        //<b> bold text <i> bold and italic </i> just bold </b>
+        Queue<HtmlTag> tags = new LinkedList<>();
+        tags.add(new HtmlTag("b", true));
+        tags.add(new HtmlTag("i", true));
+        tags.add(new HtmlTag("i", false));
+        tags.add(new HtmlTag("b", false));
+
+        HtmlValidator validator = new HtmlValidator(tags);
+
+        Assert.assertEquals(expectedOutputToString("expected_output/validate_result_for_test1.txt"),
+                validatorOutputToString(validator));
+
 
 	}
 
@@ -92,6 +106,17 @@ public class HtmlValidatorTest {
      */
 	@Test
 	public void test2(){
+        //<html><b> bold text <i> italic text </i> oops, forgot to close the rest
+        Queue<HtmlTag> qTest = new LinkedList<>();
+        qTest.add(new HtmlTag("html", true));
+        qTest.add(new HtmlTag("b", true));
+        qTest.add(new HtmlTag("i", true));
+        qTest.add(new HtmlTag("i", false));
+
+        HtmlValidator validator = new HtmlValidator(qTest);
+
+        Assert.assertEquals(expectedOutputToString("expected_output/validate_result_for_test2.txt"),
+                validatorOutputToString(validator));
 
 	}
 
@@ -103,7 +128,18 @@ public class HtmlValidatorTest {
      */
 	@Test
 	public void test3(){
+        //<b> bold text <i> bold and italic </b> oops, closed bold first </i>
+        Queue<HtmlTag> qTest = new LinkedList<>();
 
+        qTest.add(new HtmlTag("b", true));
+        qTest.add(new HtmlTag("i", true));
+        qTest.add(new HtmlTag("b", false));
+        qTest.add(new HtmlTag("i", false));
+
+        HtmlValidator validator = new HtmlValidator(qTest);
+
+        Assert.assertEquals(expectedOutputToString("expected_output/validate_result_for_test3.txt"),
+                validatorOutputToString(validator));
 	}
 
 
@@ -114,8 +150,23 @@ public class HtmlValidatorTest {
      */
 	@Test
 	public void test4(){
+        //<b> some text <i> </b> </i> </b> </html>
+        Queue<HtmlTag> qTest = new LinkedList<>();
 
-	}
+        qTest.add(new HtmlTag("b", true));
+        qTest.add(new HtmlTag("i", true));
+        qTest.add(new HtmlTag("b", false));
+        qTest.add(new HtmlTag("i", false));
+        qTest.add(new HtmlTag("b", false));
+        qTest.add(new HtmlTag("html", false));
+
+        HtmlValidator validator = new HtmlValidator(qTest);
+
+        Assert.assertEquals(expectedOutputToString("expected_output/validate_result_for_test4.txt"),
+                validatorOutputToString(validator));
+
+
+    }
 
     /**
      * This test5 method should test your validate() method
@@ -124,8 +175,16 @@ public class HtmlValidatorTest {
      */
 	@Test
 	public void test5(){
+        //</html>
+        Queue<HtmlTag> qTest = new LinkedList<>();
+        qTest.add(new HtmlTag("html", false));
 
-	}
+        HtmlValidator validator = new HtmlValidator(qTest);
+
+        Assert.assertEquals(expectedOutputToString("expected_output/validate_result_for_test5.txt"),
+                validatorOutputToString(validator));
+
+    }
 
     /**
      * This test1 method should test your validate() method
@@ -134,6 +193,14 @@ public class HtmlValidatorTest {
      */
 	@Test
 	public void test6(){
+        //This file intentionally has no tags in it.
+        Queue<HtmlTag> qTest = new LinkedList<>();
+        //qTest.add(new HtmlTag("html", false));
+
+        HtmlValidator validator = new HtmlValidator(qTest);
+
+        Assert.assertEquals(expectedOutputToString("expected_output/validate_result_for_test6.txt"),
+                validatorOutputToString(validator));
 
 	}
 
@@ -144,6 +211,47 @@ public class HtmlValidatorTest {
      */
 	@Test
 	public void test7(){
+        //<!doctype html public "-//W3C//DTD HTML 4.01 Transitional//EN">
+        //<!-- Valid example from spec -->
+        //<html>
+        //  <head>
+        //    <title>Turtles are cool</title>
+        //    <meta http-equiv="Content-Type" content="text/html">
+        //    <link href="style.css" type="text/css" />
+        //  </head>
+        //
+        //  <body>
+        //    <p>Turtles swim in the <a href="http://ocean.com/">ocean</a>.</p>
+        //    <p>Some turtles are over 100 years old.  Here is a picture of a turtle:
+        //       <img src="images/turtle.jpg" width="100" height="100">
+        //    </p>
+        //  </body>
+        //</html>
+        Queue<HtmlTag> qTest = new LinkedList<>();
+        qTest.add(new HtmlTag("!doctype", true));
+        qTest.add(new HtmlTag("!--", true));
+        qTest.add(new HtmlTag("html", true));
+        qTest.add(new HtmlTag("head", true));
+        qTest.add(new HtmlTag("title", true));
+        qTest.add(new HtmlTag("title", false));
+        qTest.add(new HtmlTag("meta", true));
+        qTest.add(new HtmlTag("link", true));
+        qTest.add(new HtmlTag("head", false));
+        qTest.add(new HtmlTag("body", true));
+        qTest.add(new HtmlTag("p", true));
+        qTest.add(new HtmlTag("a", true));
+        qTest.add(new HtmlTag("a", false));
+        qTest.add(new HtmlTag("p", false));
+        qTest.add(new HtmlTag("p", true));
+        qTest.add(new HtmlTag("img", true));
+        qTest.add(new HtmlTag("p", false));
+        qTest.add(new HtmlTag("body", false));
+        qTest.add(new HtmlTag("html", false));
+
+        HtmlValidator validator = new HtmlValidator(qTest);
+
+        Assert.assertEquals(expectedOutputToString("expected_output/validate_result_for_test7.txt"),
+                validatorOutputToString(validator));
 
 	}
 
@@ -154,7 +262,49 @@ public class HtmlValidatorTest {
      */
 	@Test
 	public void test8(){
+        //<!doctype html public "-//W3C//DTD HTML 4.01 Transitional//EN">
+        //<!-- Invalid example from spec -->
+        //<html>
+        //  <head>
+        //    <title>Turtles are cool
+        //    <meta http-equiv="Content-Type" content="text/html">
+        //    <link href="style.css" type="text/css" rel="stylesheet" />
+        //  </head>
+        //  </head>
+        //
+        //  <body>
+        //    <p>Turtles swim in the <a href="http://ocean.com>ocean</a>.</p>
+        //    </br>
+        //    <p>Some turtles are over 100 years old.  Here is a picture of a turtle:
+        //       <img src="images/turtle.jpg" width="100" height="100"> </p>
+        //</html>
 
+        Queue<HtmlTag> qTest = new LinkedList<>();
+        qTest.add(new HtmlTag("!doctype", true));
+        qTest.add(new HtmlTag("!--", true));
+        qTest.add(new HtmlTag("html"));
+        qTest.add(new HtmlTag("head", true));
+        qTest.add(new HtmlTag("title", true));
+        qTest.add(new HtmlTag("meta", true));
+        qTest.add(new HtmlTag("link", true));
+        qTest.add(new HtmlTag("head", false));
+        qTest.add(new HtmlTag("head", false));
+        qTest.add(new HtmlTag("body", true));
+        qTest.add(new HtmlTag("p", true));
+        qTest.add(new HtmlTag("a", true));
+        qTest.add(new HtmlTag("a", false));
+        qTest.add(new HtmlTag("p", false));
+        qTest.add(new HtmlTag("br", false));
+        qTest.add(new HtmlTag("p", true));
+        qTest.add(new HtmlTag("img", true));
+        qTest.add(new HtmlTag("p", false));
+        qTest.add(new HtmlTag("html", false));
+
+
+        HtmlValidator validator = new HtmlValidator(qTest);
+
+        Assert.assertEquals(expectedOutputToString("expected_output/validate_result_for_test8.txt"),
+                validatorOutputToString(validator));
 	}
 
 	/**
@@ -193,5 +343,6 @@ public class HtmlValidatorTest {
 
     }
 
-    //FEEL FREE TO ADD MORE TESTS HERE
 }
+
+
